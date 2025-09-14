ebc75e8606c39ff89cdefa6a453d4c48ab139b27
Gabriel Syphan <syphan@Gabriels-MacBook-Air.local>
2025-09-14 18:50:18 -0300

    feat: Interface implementation

--- src/main/kotlin/com/syphan/financial/app/controller/web/FinancesController.kt
+++ src/main/kotlin/com/syphan/financial/app/controller/web/FinancesController.kt
@@ -1,12 +1,24 @@
 package com.syphan.financial.app.controller.web
 
+import com.syphan.financial.domain.usecase.FindAllCardsUseCase
+import com.syphan.financial.domain.usecase.FindAllTransactionsUseCase
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/finances")
-class FinancesController {
+class FinancesController(
+    private val findAllTransactionsUseCase: FindAllTransactionsUseCase,
+    private val findAllCardsUseCase: FindAllCardsUseCase,
+) {
     @GetMapping
-    fun index(): String = "finances/index"
+    fun index(model: Model): String {
+        val transactions = findAllTransactionsUseCase.execute()
+        val cards = findAllCardsUseCase.execute()
+        model.addAttribute("transactions", transactions)
+        model.addAttribute("cards", cards)
+        return "finances/index"
+    }
 }
--- src/main/kotlin/com/syphan/financial/domain/entity/enums/CardType.kt
+++ src/main/kotlin/com/syphan/financial/domain/entity/enums/CardType.kt
@@ -1,9 +1,11 @@
 package com.syphan.financial.domain.entity.enums
 
-enum class CardType {
-    CREDIT,
-    DEBIT,
-    PREPAID,
-    CHARGE,
-    VIRTUAL,
+enum class CardType(
+    val friendlyName: String,
+) {
+    CREDIT("Credit"),
+    DEBIT("Debit"),
+    PREPAID("Prepaid"),
+    CHARGE("Charge"),
+    VIRTUAL("Virtual"),
 }
